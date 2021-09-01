cfba41fed1c442b93c71ee1bcae1e4b27d82505f
Pavel Boltromyuk <p.boltromyuk@vironit.co.uk>
2021-09-01 14:23:02 +0300

    added photo message type

---
+++ Chat/Chat/Helpers/Chat/ImageMediaItem.swift
@@ -0,0 +1,22 @@
+//
+//  ImageMediaItem.swift
+//  Chat
+//
+//  Created by User on 9/1/21.
+//
+
+import Foundation
+import MessageKit
+
+struct ImageMediaItem: MediaItem {
+  var url: URL?
+  var image: UIImage?
+  var placeholderImage: UIImage
+  var size: CGSize
+
+  init(image: UIImage) {
+    self.image = image
+    self.size = CGSize(width: 240, height: 240)
+    self.placeholderImage = UIImage()
+  }
+}
--- Chat/Chat/Utils/Extensions/TdLib/MessagesExtension.swift
+++ Chat/Chat/Utils/Extensions/TdLib/MessagesExtension.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import TDLib
+import UIKit
 
 extension Messages {
     func convertToArrayMessages() -> [Message] {
@@ -14,17 +15,26 @@ extension Messages {
         
         if totalCount > 0 {
             for item in self.messages! {
-                let userId = item.senderUserId
+                let userId = String(item.senderUserId)
                 var content = ""
+                var image: UIImage?
                 switch item.content {
                 case .messageText(text: let text, webPage: nil):
                     content = text.text ?? "default"
+                case .messagePhoto(photo: let photo, caption: let caption, isSecret: false):
+                    content = caption.text ?? ""
+                    if let data = photo.minithumbnail?.data {
+                        image = UIImage(data: data)
+                    }
+                case .messageSticker(sticker: let sticker):
+                    content = sticker.emoji
                 default:
-                    print("Default")
+                    print("Content: \(item.content)")
                 }
                 let date = item.date
                 let id = item.id
-                let message = Message(id: id, userId: userId, content: content, displayName: "test", date: date)
+                let sender = Sender(senderId: userId, displayName: "Test")
+                let message = Message(id: id, sender: sender, content: content, date: date, image: image)
                 messages.append(message)
             }
             return messages.reversed()
